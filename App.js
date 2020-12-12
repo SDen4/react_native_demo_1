2121e6b83bae4bfc844ba72ba5a69d2c9b64d700
SDen4 <asw325@yandex.ru>
2020-12-12 20:31:34 +0500

    navbar component

--- App.js
+++ App.js
@@ -1,30 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Navbar } from './src/Navbar';
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>Hello test!!!</Text>
-
-            {/* inline style */}
-            <Text style={ {color: 'white'} }>Second row</Text>
-            <Text style={styles.third}>The third row</Text>
-            <StatusBar style="auto" />
+            <Navbar />
         </View>
     );
 }
 
-//style using /StyleSheet' component and its method 'create'
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'steelblue',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    third: {
-        fontSize: 24,
-        color: 'brown',
-    }
+    container: {}
 });
