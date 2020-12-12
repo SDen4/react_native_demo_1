4979e27991baae00d49af0c3b488fdfc50fd7176
SDen4 <asw325@yandex.ru>
2020-12-12 19:39:50 +0500

    types of styles

--- App.js
+++ App.js
@@ -3,19 +3,28 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>Hello test!!!</Text>
+
+            {/* inline style */}
+            <Text style={ {color: 'white'} }>Second row</Text>
+            <Text style={styles.third}>The third row</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
+//style using /StyleSheet' component and its method 'create'
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'steelblue',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    third: {
+        fontSize: 24,
+        color: 'brown',
+    }
 });
