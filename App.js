4c768cacb80dd062b03cb9c84568e972d0ef2a45
SDen4 <asw325@yandex.ru>
2020-12-13 23:19:48 +0500

    ScrollView

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
 import { AddTodo } from './src/AddTodo';
 import { Navbar } from './src/Navbar';
 import { Todo } from './src/Todo';
 
 export default function App() {
-    const [todos, setTodos] = useState([]);
+    const [todos, setTodos] = useState([
+        {id: 1, title: 'test'},
+        {id: 2, title: 'test'},
+        {id: 3, title: 'test'},
+        {id: 4, title: 'test'},
+        {id: 5, title: 'test'},
+        {id: 6, title: 'test'},
+        {id: 7, title: 'test'},
+        {id: 8, title: 'test'},
+        {id: 9, title: 'test'},
+        {id: 10, title: 'test'},
+        {id: 12, title: 'test'},
+        {id: 13, title: 'test'},
+        {id: 14, title: 'test'},
+        {id: 15, title: 'test'},
+        {id: 16, title: 'test'},
+        {id: 17, title: 'test'},
+    ]);
 
     const addTodo = (title) => {
         setTodos((prev) => [
@@ -18,23 +35,26 @@ export default function App() {
     };
 
     return (
-        <View>
-            <Navbar title="Todo App!" />
+        <View style={styles.main}>
+            <Navbar title="Todo App" />
             <View style={styles.container}>
                 <AddTodo onSubmit={addTodo} />
-                <View>
+                <ScrollView>
                     {todos.map((todo) => (
                         <Todo key={todo.id} todo={todo} />
                     ))}
-                </View>
+                </ScrollView>
             </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    main: {
+        flex: 1,
+    },
     container: {
         paddingHorizontal: 10,
         paddingVertical: 10,
-    },
+    }
 });
