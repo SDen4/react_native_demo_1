4bac959055562d86604da52ea1c1fcbcd0f762df
SDen4 <asw325@yandex.ru>
2020-12-15 22:38:35 +0500

    TodoScreen functionality

--- App.js
+++ App.js
@@ -2,45 +2,21 @@ import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Navbar } from './src/components/Navbar';
 import { MainScreen } from './src/screens/MainScreen';
+import { TodoScreen } from './src/screens/TodoScreen';
 
 export default function App() {
-    // const [todos, setTodos] = useState([
-    //     { id: '1', title: 'test' },
-    //     { id: '2', title: 'test' },
-    //     { id: '3', title: 'test' },
-    //     { id: '4', title: 'test' },
-    //     { id: '5', title: 'test' },
-    //     { id: '6', title: 'test' },
-    //     { id: '7', title: 'test' },
-    //     { id: '8', title: 'test' },
-    //     { id: '9', title: 'test' },
-    //     { id: '10', title: 'test' },
-    //     { id: '12', title: 'test' },
-    //     { id: '13', title: 'test' },
-    //     { id: '14', title: 'test' },
-    //     { id: '15', title: 'test' },
-    //     { id: '16', title: 'test' },
-    //     { id: '17', title: 'test' },
-    //     { id: '18', title: 'test' },
-    //     { id: '19', title: 'test' },
-    //     { id: '20', title: 'test' },
-    //     { id: '21', title: 'test' },
-    //     { id: '22', title: 'test' },
-    //     { id: '23', title: 'test' },
-    //     { id: '24', title: 'test' },
-    //     { id: '25', title: 'test' },
-    //     { id: '26', title: 'test' },
-    //     { id: '27', title: 'Last' },
-    // ]);
-
-    const [todos, setTodos] = useState([]);
+    const [todoId, setTodoId] = useState(null);
+    const [todos, setTodos] = useState([
+        { id: '1', title: 'Выучить React Native' },
+        { id: '2', title: 'Написать приложение' }
+    ])
 
     const addTodo = (title) => {
         setTodos((prev) => [
             ...prev,
             {
                 id: Date.now().toString(),
-                title,
+                title
             },
         ]);
     };
@@ -49,12 +25,24 @@ export default function App() {
         setTodos((prev) => prev.filter((item) => item.id !== id));
     };
 
+    let content = (
+        <MainScreen
+            todos={todos}
+            addTodo={addTodo}
+            removeTodo={removeTodo}
+            openTodo={setTodoId}
+        />
+    );
+
+    if (todoId) {
+        const selectedTodo = todos.find(todo => todo.id === todoId)
+        content = <TodoScreen goBack={() => setTodoId(null)} todo={selectedTodo} />
+      }
+
     return (
         <View style={styles.main}>
             <Navbar title="Todo App" />
-            <View style={styles.container}>
-                <MainScreen todos={todos} addTodo={addTodo} removeTodo={removeTodo}/>
-            </View>
+            <View style={styles.container}>{content}</View>
         </View>
     );
 }
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -3,15 +3,15 @@ import { StyleSheet, View, FlatList } from 'react-native';
 import { AddTodo } from '../components/AddTodo';
 import { Todo } from '../components/Todo';
 
-export const MainScreen = ({ addTodo, todos, removeTodo }) => {
+export const MainScreen = ({ addTodo, todos, removeTodo, openTodo }) => {
     return (
         <View>
             <AddTodo onSubmit={addTodo} />
             <FlatList
-                keyExtractor={(item) => item.id}
+                keyExtractor={(item) => item.id.toString()}
                 data={todos}
                 renderItem={({ item }) => {
-                    return <Todo todo={item} onRemove={removeTodo} />;
+                    return <Todo todo={item} onRemove={removeTodo} onOpen={openTodo}/>;
                 }}
             />
         </View>
