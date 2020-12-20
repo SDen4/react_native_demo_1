dee4fa0c722f7f6e9ecdeb3fa9baccd396c8d360
SDen4 <asw325@yandex.ru>
2020-12-20 16:11:33 +0500

    reducer refactoring

--- src/context/todo/todoReducer.js
+++ src/context/todo/todoReducer.js
@@ -1,28 +1,33 @@
 import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from '../types';
 
+const handlers = {
+    [ADD_TODO]: (state, { title }) => ({
+        ...state,
+        todos: [
+            ...state.todos,
+            {
+                id: Date.now().toString(),
+                title,
+            },
+        ],
+    }),
+    [REMOVE_TODO]: (state, { id }) => ({
+        ...state,
+        todos: state.todos.filter((item) => item.id !== id),
+    }),
+    [UPDATE_TODO]: (state, {title, id}) => ({
+        ...state,
+        todos: state.todos.map((item) => {
+            if (item.id === id) {
+                item.title = title;
+            }
+            return item;
+        }),
+    }),
+    DEFAULT: state => state
+};
+
 export const todoReducer = (state, action) => {
-    switch (action.type) {
-        case ADD_TODO:
-            return {
-                ...state,
-                todos: [
-                    ...state.todos,
-                    {
-                        id: Date.now().toString(),
-                        title: action.title,
-                    },
-                ],
-            };
-        case UPDATE_TODO:
-            return { ...state, todos: state.todos.map(item => {
-                if (item.id === action.id) {
-                    item.title = action.title;
-                };
-                return item;
-            })};
-        case REMOVE_TODO:
-            return { ...state, todos: state.todos.filter((item) => item.id !== action.id) };
-        default:
-            return state;
-    }
+    const handler = handlers[action.type] || handlers.DEFAULT;
+    return handler(state, action);
 };
